b18e5ea31b3c9151b007bd43cc10d3248a4ea2cb
riyajainn01 <jriya9602@gmail.com>
2025-04-18 21:38:42 +0530

    added files

--- backend/venv/Scripts/activate.bat
+++ backend/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\dell\OneDrive\Desktop\jklu\housewise-predictor-32\backend\venv
+set VIRTUAL_ENV=C:\Users\LENOVO\OneDrive\Desktop\project(py+react)\housewise-predictor-32\backend\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
